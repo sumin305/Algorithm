4d56eefabccbf5676466fb4b056026ccc3db6a91
SOOM <leesumin305@gmail.com>
2023-01-13 10:18:08 +0900

    Update 하지만 빨랐죠.swift
    최대 공약수 반환하는 함수 구현

--- Coding Test/하지만 빨랐죠.swift
+++ Coding Test/하지만 빨랐죠.swift
@@ -12,7 +12,6 @@ for i in 1...Int(readLine()!)!{
         Int(String($0))!
     }
     let (a,b) = (n[0], n[1])
-    //최소 공배수 구하는 법 ?  준식이는 두 수의 곱을 최소 공배수로 구한다
     if GetLCM(A : max(a,b), B : min(a,b)) == 1 {
         print("Perfect")
     }else{
