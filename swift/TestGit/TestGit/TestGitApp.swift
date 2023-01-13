84e59dc45abe10320bc3de643aaae941a6cf2f59
sumin305 <gogotnals@naver.com>
2023-01-13 11:30:47 +0900

    test

---
+++ swift/TestGit/TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by 이수민 on 2023/01/13.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
